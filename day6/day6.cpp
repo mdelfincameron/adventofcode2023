7bf27cca84bbb10327f1d6cf0d29214ce48b2fa2
Michael Cameron <mdelfincameron@gmail.com>
2023-12-06 20:57:03 -0800

    Finished day 6 part 1

--- day6/day6.cpp
+++ day6/day6.cpp
@@ -22,10 +22,23 @@ vector<string> split(string str, string delimiter){
     return res;
 }
 
+vector<int> convertToInt(vector<string> v){
+    vector<int> res;
+    for(auto i : v){
+        if(i != ""){
+            res.push_back(stoi(i));
+        }
+    }
+    return res;
+}
+
 int main(){
     string filename;
     fstream file;
-    long sum = 0;
+    long long total = 1;
+    vector<int> winCounts;
+    vector<int> times;
+    vector<int> distances;
 
     //Get filename, open file
     cout << "Please input filename: ";
@@ -34,61 +47,51 @@ int main(){
     auto start = chrono::steady_clock::now();
     file.open(filename,ios::in);
 
-    if(file.is_open()){
-
-        //Read line by line through file
-        string line;
-        while(getline(file,line)){
-
-            int id = 0;
-            int matchCount = 0;
-
-            //Split along colon
-            vector<string> splitLine = split(line,":");
-
-            //Get number of card
-            smatch m;
-            regex digits = regex("\\d+");
-            regex_search(splitLine[0],m,digits);
-            id = stoi(m.str());
-
-            //Split along pipe symbol
-            vector<string> splitNums = split(splitLine[1],"|");
-
-            //Extract numbers, put them in a vector
-            vector<int> winningNums = convertToInt(split(splitNums[0]," "));
-            vector<int> haveNums = convertToInt(split(splitNums[1]," "));
-
-            /*
-            cout << "Winning Numbers: ";
-            for(auto i : winningNums){
-                cout << i << " ";
-            }
-            cout << endl;
-            cout << "Have Numbers: ";
-            for(auto i : haveNums){
-                cout << i << " ";
-            }
-            cout << endl;
-            */
-
-           //Find how many matches there are in winning number vector, add appropriate amount to sum
-            for(auto i : haveNums){
-                if(find(winningNums.begin(), winningNums.end(), i) != winningNums.end()){
-                    matchCount++;
-                }
-            }
-            cout << id << " " << matchCount << endl;
-
-            sum += pow(2, matchCount - 1);
-        }
+    if(!file.is_open()){
+        exit(1);
     }
 
+    //Read lines in
+    string line;
+
+    getline(file,line);
+    vector<string> splitLine = split(line,":");
+    times = convertToInt(split(splitLine[1]," "));
+
+    getline(file,line);
+    splitLine = split(line,":");
+    distances = convertToInt(split(splitLine[1]," "));
+
+    for(int i = 0; i < times.size(); i++){
+        int minTime, maxTime;
+        int timeAllowed = times[i];
+        int record = distances[i];
+        for(int j = 1; j < timeAllowed; j++){
+            if(j * (timeAllowed - j) > record){
+                minTime = j;
+                break;
+            }
+        }
+
+        for(int j = timeAllowed - 1; j > 0; j--){
+            if(j * (timeAllowed - j) > record){
+                maxTime = j;
+                break;
+            }
+        }
+
+        winCounts.push_back(maxTime - minTime + 1);
+
+    }
     file.close();
+
+    for(auto win : winCounts){
+        total *= win;
+    }
 
     auto end = chrono::steady_clock::now();
 
-    cout << sum << endl;
+    cout << total << endl;
     cout << "Runtime is " << chrono::duration <double, milli> (end - start).count() << " ms" << endl; 
 
     exit(0);
